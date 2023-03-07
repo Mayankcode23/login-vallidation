c215186d7e0a8238deb7412db8ebdaee9f467556
Mayankcode23 <127024990+Mayankcode23@users.noreply.github.com>
2023-03-07 12:31:43 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+// login form validation
+
+ import './App.css';
+ import Login from './Login';
+ function App(){
+   return(
+    <div>
+      <Login />
+    </div>
+
+  );
+ }
+ export default App;
